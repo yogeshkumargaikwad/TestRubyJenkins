233cf5c15607dd36341dc2dcbf4f8501022f7905
Yogeshkumar Gaikwad <yogeshkumar.gaikwad@enzigma.in>
2018-01-10 22:50:27 +0530

    Committed first spec file to git for jenkins test

---
+++ test_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+class Test
+  def initialize
+    puts 'Hello Enzigma!'
+  end
+end
+describe 'Test behaviour' do
+  it 'should display message' do
+    puts 'In display message test case'
+    objActualTest = Test.new
+    strExpectedMsg = "Hello Enzigma!"
+    strExpectedMsg.eql?(objActualTest)
+  end
+end
