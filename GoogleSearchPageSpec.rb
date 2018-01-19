79637937f40e858f260d8695379aeaed81498ecf
yogeshkumargaikwad <yogeshkumar.gaikwad@enzigma.in>
2018-01-19 22:43:43 +0530

    Update GoogleSearchPageSpec.rb

--- GoogleSearchPageSpec.rb
+++ GoogleSearchPageSpec.rb
@@ -5,7 +5,7 @@ describe "Google Search" do
   before(:each) do
     puts 'In Google Search Page Spec Before'
     puts ENV['JENKINS_HOME']+"/chromedriver.exe"    
-    Selenium::WebDriver::Chrome.driver_path = ENV['JENKINS_HOME']+"/chromedriver.exe";
+    Selenium::WebDriver::Chrome.driver_path = ENV['JENKINS_HOME']+"/chromedriver";
     puts Selenium::WebDriver::Chrome.driver_path.to_s()
     @driver = Selenium::WebDriver.for(:chrome)
     @base_url = "https://www.google.co.in/"
