20d5b6e66fb4a9e739dfb643201197f7d010c3a7
yogeshkumargaikwad <yogeshkumar.gaikwad@enzigma.in>
2018-02-09 10:00:41 +0530

    Updated spec executor

--- SpecManager/specExecutor_spec.rb
+++ SpecManager/specExecutor_spec.rb
@@ -2,15 +2,15 @@ require 'rspec'
 describe "test" do
   it "test1" do
     if !ENV['PROJECT_ID'].nil? && ENV['SUIT_ID'].nil? && ENV['SECTION_ID'].nil? then
-      Kernel.exec("ruby specManager.rb project:#{ENV['PROJECT_ID']}")
+      Kernel.exec("ruby SpecManager/specManager.rb project:#{ENV['PROJECT_ID']}")
     else
       if !ENV['SUIT_ID'].nil? && ENV['PROJECT_ID'].nil? && ENV['SECTION_ID'].nil? then
-        Kernel.exec("ruby specManager.rb suit:#{ENV['SUIT_ID']}")
+        Kernel.exec("ruby SpecManager/specManager.rb suit:#{ENV['SUIT_ID']}")
       else
         if !ENV['SECTION_ID'].nil? && ENV['PROJECT_ID'].nil? && ENV['SUIT_ID'].nil? then
-          Kernel.exec("ruby specManager.rb section:#{ENV['SECTION_ID']}")
+          Kernel.exec("ruby SpecManager/specManager.rb section:#{ENV['SECTION_ID']}")
         else
-          Kernel.exec("ruby specManager.rb project:#{ENV['PROJECT_ID']} suit:#{ENV['SUIT_ID']} section:#{ENV['SECTION_ID']}")
+          Kernel.exec("ruby SpecManager/specManager.rb project:#{ENV['PROJECT_ID']} suit:#{ENV['SUIT_ID']} section:#{ENV['SECTION_ID']}")
         end
       end
     end
